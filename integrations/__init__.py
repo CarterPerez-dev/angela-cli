6cdd5e9c7fc2266013d88818f96434c761c4bea7
Yoshi2003 <cartertechsolution@gmail.com>
2025-05-11 03:40:52 -0400

    minor changes

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -7,8 +7,8 @@ extending functionality and connecting subsystems that would otherwise create
 circular dependencies.
 """
 
-from .semantic_integration import semantic_integration
+
 from .phase12_integration import phase12_integration
 
 
-__all__ = ['semantic_integration', 'phase12_integration']
+__all__ = ['phase12_integration']
